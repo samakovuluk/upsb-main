b71b589a126d25c574be4e8e9ce333a53d5772e5
Undeath <uluk.samakov@gmail.com>
2021-06-06 10:37:13 +0600

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 module.exports = (app) => {
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 };
